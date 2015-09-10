dda8d7ec6a1a44c79bb54cff20be67d29bc2171c
pinkybs <zhangxin_sk@qq.com>
2015-09-10 11:04:00 +0800

    update to latest

---
+++ ilasik/src/js/common-en.js
@@ -0,0 +1,338 @@
+var _DATA = [{"id":1,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing AIMASHI eye ilasik central","site":"www.amass-eye.com"},{"id":2,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing AIMEIAO medical science company","site":"www.aimeiao.com"},{"id":3,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing HUADE eye hospital","site":"www.bjpryk.com"},{"id":4,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing HUADE eye hospital company","site":"1213109.czvv.com"},{"id":5,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing HUADU asia hospital","site":"www.kq36.cn\/324eb3ib5"},{"id":6,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing JIAHUI international eye central","site":"www.kq36.cn\/107207"},{"id":7,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing JIAYUE eyehospital","site":"www.jiayuechina.com"},{"id":8,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing military hospital","site":"www.bjjqzyy.com"},{"id":9,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing MINZHONG eyehospital","site":"www.mzeyes.com"},{"id":10,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing MIN sight eye hospital","site":"www.mingshiguang.cn"},{"id":11,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing RUNSHI eye hospital","site":"www.runshistar.com"},{"id":12,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"BeiJing TONGREN FUKANG eye hospital","site":"556506.pe168.com"},{"id":13,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"Beijing TONGREN hospital","site":"www.trhos.com"},{"id":14,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"Beijing XINLI eye hospital","site":"www.bjxlyk.com"},{"id":15,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"HAOHAO eye","site":"yiyuan.zgjsyw.com"},{"id":16,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"Chinese People's Liberation Army 261 hospital","site":"www.gyh261.com"},{"id":17,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"Chinese People's Liberation Army 306 hospital","site":"www.306.cn"},{"id":18,"cityKey":"BEIJING","cityName":"BeiJing","hospital":"Chinese People's Liberation Army 309 hospital","site":"www.309yy.com"},{"id":19,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"Chinese People's Liberation Army 85 hospital","site":"www.sh85yy.com"},{"id":20,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai AIER eye hospital company","site":"www.aier021.com"},{"id":21,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai HUSHEN ENT dept hospital","site":"www.hs5g.com"},{"id":22,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai JIAOTONG university affiliated XINHUA hospital","site":"www.xinhuamed.com.cn"},{"id":23,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai MINGSHIKANG eye hospital(Chinese People's Liberation Army 455 hospital)","site":"www.455hospital.cn"},{"id":24,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai PURUI eye hospital","site":"www.pr021.com"},{"id":25,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai RENAI hospital eye","site":"www.renai.cn"},{"id":26,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai eye diseases prevention central","site":"www.shsyf.com"},{"id":27,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai new eye sight hospital","site":"www.shneweye.com"},{"id":28,"cityKey":"SHANGHAI","cityName":"ShangHai","hospital":"ShangHai CHANGHAI hospital","site":"chhospital.com.cn"},{"id":29,"cityKey":"GUANGZHOU","cityName":"GuangZhou","hospital":"AMASHI eye hospital","site":"www.amass-eye.com"},{"id":30,"cityKey":"GUANGZHOU","cityName":"GuangZhou","hospital":"GUANGDONG people hospital","site":"www.gdghospital.org.cn"},{"id":31,"cityKey":"GUANGZHOU","cityName":"GuangZhou","hospital":"GuangZhou AIER eyehospital","site":"www.eye020.com"},{"id":32,"cityKey":"GUANGZHOU","cityName":"GuangZhou","hospital":"GuangZhou military GuangZhou hospital","site":"www.gzzyy.com"},{"id":33,"cityKey":"GUANGZHOU","cityName":"GuangZhou","hospital":"GuangZhou first people hospital","site":"yy.gzhosp.cn"},{"id":34,"cityKey":"GUANGZHOU","cityName":"GuangZhou","hospital":"GuangZhou JILI eye clinic","site":"www.jilieye.com"},{"id":35,"cityKey":"GUANGZHOU","cityName":"GuangZhou","hospital":"ZHONGSHAN university eye central","site":"www.gzzoc.com"},{"id":36,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"Beijing university ShenZhen hospital","site":"www.pkuszh.com"},{"id":37,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"ShenZhen BOAI hospital","site":"www.szboai.com.cn"},{"id":38,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"ShenZhen spring hospital","site":"spring.91.cn"},{"id":39,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"ShenZhen KEMEI eye hospital","site":"www.kemei.cn"},{"id":40,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"ShenZhen eye hospital","site":"www.sz-eyes.com"},{"id":41,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"ShenZhen SHUGUANG eye hospital","site":"yanke.sg91.net"},{"id":42,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"ShenZhen SHUGUANG hospital","site":"www.sg91.net\/"},{"id":43,"cityKey":"SHENZHEN","cityName":"ShenZhen","hospital":"ShenZhen sun shine hospital","site":"sun.91.cn"},{"id":44,"cityKey":"WUHAN","cityName":"WuHan","hospital":"WUCHANG AIGE eye hospital","site":"s9mvsm0v.locoso.com"},{"id":45,"cityKey":"WUHAN","cityName":"WuHan","hospital":"WuHan AIGE eye hospital","site":"www.aige010.com"},{"id":46,"cityKey":"WUHAN","cityName":"WuHan","hospital":"WuHan AIER eye hospital","site":"www.eye027.com"},{"id":47,"cityKey":"WUHAN","cityName":"WuHan","hospital":"WuHan RUIPU eye hospital","site":"www.pr027.com"},{"id":48,"cityKey":"CHENGDU","cityName":"ChengDu","hospital":"ChengDu 363 hospital","site":"www.hospital2.com\/hostemp_110822\/"},{"id":49,"cityKey":"CHENGDU","cityName":"ChengDu","hospital":"ChengDu AIDE eye hospital","site":"www.028aidi.com"},{"id":50,"cityKey":"CHENGDU","cityName":"ChengDu","hospital":"ChengDu AIER eye hospital","site":"cdeye.ecaihr.com"},{"id":51,"cityKey":"CHENGDU","cityName":"ChengDu","hospital":"ChengDu KANGQIAO eye hospital","site":"www.kqeye.com"},{"id":52,"cityKey":"CHENGDU","cityName":"ChengDu","hospital":"ChengDu LUOJI air force hospital(452)","site":"www.chinaeye.com"},{"id":53,"cityKey":"CHENGDU","cityName":"ChengDu","hospital":"ChengDu RUIPU eye hospital","site":"www.p028.com"},{"id":54,"cityKey":"CHENGDU","cityName":"ChengDu","hospital":"ChengDu century eye hospital","site":"www.cdsjyk.com"},{"id":55,"cityKey":"NANJING","cityName":"NanJing","hospital":"NanJing AIER eye hospital","site":"www.eye025.com"},{"id":56,"cityKey":"NANJING","cityName":"NanJing","hospital":"NanJing DONGNAN eye hospital","site":"www.njsee.com"},{"id":57,"cityKey":"NANJING","cityName":"NanJing","hospital":"NanJing medicine university second affiliated hospital","site":"www.jsnydefy.com"},{"id":58,"cityKey":"HANGZHOU","cityName":"HangZhou","hospital":"HangZhou MINGSHIKANG eye hospital","site":"www.msk-lasik.com\/"},{"id":59,"cityKey":"HANGZHOU","cityName":"HangZhou","hospital":"HangZhou teacher training university affiliated hospital(second people hospital)","site":"www.hz2y.com"},{"id":60,"cityKey":"HANGZHOU","cityName":"HangZhou","hospital":"XIAOSHAN laser hospital","site":"www.xsjgyy.com"},{"id":61,"cityKey":"HANGZHOU","cityName":"HangZhou","hospital":"ZHEJIANG people hospital","site":"www.hospitalstar.com"}];
+
+var ilasik = {
+  test: function(){
+	//ilasik.setCookie("lang","cn",2592000);
+	//alert(ilasik.getCookie('lang'));
+	//ilasik.clearCookie("lang");                 
+  },
+  initLang: function(){	  
+	//console.debug(ilasik.getCookie('lang'));
+	$('.logo .language').delegate(".chinese","click",function(){
+		ilasik.setCookie("lang","cn",2592000);
+		//console.debug(window.location.href);
+		$(this).attr('href', window.location.href.replace("/en/","/"));
+	});
+	$('.logo .language').delegate(".english","click",function(e){
+		ilasik.setCookie("lang","en",2592000);
+		if (window.location.href.lastIndexOf('/en/') == -1) {			
+			var idx = window.location.href.lastIndexOf('/');
+			$(this).attr('href', window.location.href.substr(0,idx+1) + 'en' +  window.location.href.substr(idx));
+		}
+		else {
+			$(this).attr('href', window.location.href);
+		}
+	});
+	$('.mainLink .mLink3').attr('href', 'mailto:doctor@ilasik-amo.com');
+  },
+  
+  //取得cookie
+  getCookie: function(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');    
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];                      
+		while (c.charAt(0)==' ') {          
+			c = c.substring(1,c.length);
+		}
+		if (c.indexOf(nameEQ) == 0) {
+			return decodeURI(c.substring(nameEQ.length,c.length));
+		}
+	}
+	return false;
+  },
+  
+  //清除cookie
+  clearCookie: function(name) {
+	ilasik.setCookie(name, "", -1);
+  },
+
+  //设置cookie
+  setCookie: function(name, value, seconds) {
+	seconds = seconds || 0;
+	var expires = "";
+	if (seconds != 0 ) {
+		var date = new Date();
+		date.setTime(date.getTime()+(seconds*1000));
+		expires = "; expires="+date.toGMTString();
+	}
+	document.cookie = name+"="+encodeURI(value)+expires+"; path=/";
+  },
+
+  faqSlide: function(){
+    $('.faqItem .qu').on('click',function(e){
+      if($(this).hasClass('cur')){
+        $(this).removeClass('cur');
+        $(this).siblings('.aw').slideUp('fast','easeOutQuad');
+      } else {
+        $(this).addClass('cur');
+        $(this).siblings('.aw').slideDown('fast','easeOutQuad');
+      }
+      e.preventDefault();
+    })
+  },
+  
+  // index.html 首页maingif
+  mainslide: function(){
+    if($('.mainSlide').length > 0 ){
+      $('.mainSlide').cycle({
+  		  //fx:'scrollHorz',
+  		  slides:'> a',
+  		  timeout: 4000,
+  		  speed: 1000,
+  		  easing: 'easeOutQuad'
+  		});
+  		$('.mainSlide').hover(function(){
+  		  $(this).cycle('pause');	
+  		},function(){
+  		  $(this).cycle('resume');	
+  		})
+	  }else{
+	    return;
+	  }
+  },
+  
+  // searchonline.html 下拉搜索框
+  dropdown: function(){	
+	var _cityKeys = [];
+	var _cityNames = [];
+	
+	$(_DATA).each(function(i){
+		var data = _DATA[i];
+		if (-1 == $.inArray(data.cityKey, _cityKeys)) {
+			//console.debug(data.cityKey + '-' +data.cityName);
+			_cityKeys.push(data.cityKey);
+			_cityNames.push(data.cityName);
+			$('.tristan_select_op_ul').append('<li><a href="#">' + data.cityName + '</a></li>');
+		}
+    });
+	
+	$('.showInput').on('keyup', function(){		
+		var searchVal = $(this).val();
+		//console.debug(searchVal);
+		$('.tristan_select_op_ul').html('');
+		$(_cityNames).each(function(i){
+			if (_cityNames[i].indexOf(searchVal) != -1 || _cityKeys[i].indexOf(searchVal.toUpperCase()) != -1 ) {
+				$('.tristan_select_op_ul').append('<li><a href="#">' + _cityNames[i] + '</a></li>');
+				$('.searchbar').find('.dropdown').click();
+			}
+		});
+	});
+	
+    if($('.searchbar').length > 0 ){
+      $('.searchbar').find('.dropdown').on('click',function(e){
+        var $this = $(this);
+        var $tristanSlop = $this.siblings('.tristan_select_op');
+        var $showInput = $this.siblings('.showInput');
+
+        $tristanSlop.css({'display':'block'});
+        $tristanSlop.find('a').on('click',function(e){
+          var curValue = $(this).text();
+          $showInput.val(curValue);
+		  //alert('aa');alert(curValue);
+          $tristanSlop.css({'display':'none'});
+          e.preventDefault();
+        });
+
+        e.preventDefault();
+      });
+
+      $('.searchbar').find('.form').on('mouseleave',function(){
+        $(this).find('.tristan_select_op').css({'display':'none'});
+      })
+
+    }else{
+    	return;
+    }
+	
+	
+	$('.searchbar').delegate(".submit","click",function(){
+		var searchVal = $.trim($('.showInput').val());
+		//found
+		if (searchVal=='' || -1!=$.inArray(searchVal, _cityNames) || -1!=$.inArray(searchVal.toUpperCase(), _cityKeys)) {
+			var i = $.inArray(searchVal, _cityNames) == -1 ? $.inArray(searchVal.toUpperCase(), _cityKeys) : $.inArray(searchVal, _cityNames);
+			//alert('searchresult.html?city=' + _cityKeys[i]);
+			window.location.href = 'searchresult.html?city=' + (i==-1 ? '' : _cityKeys[i]);
+		}
+		else {
+			$('.errmsg').html('No result for your input[' + searchVal + '].<br/>Please select a city in the list or retry a input.').fadeIn(500).delay(3000).slideUp(500);
+			$(".showInput").focus(); 
+		}
+	});
+  },
+  
+  // searchresult.html
+  searchresult: function(city, curpage) {
+	var _cityMap = [];
+	var _cityKeys = [];
+	$(_DATA).each(function(i){
+		var data = _DATA[i];
+		if (-1 == $.inArray(data.cityKey, _cityKeys)) {
+			//console.debug(data.cityKey + '-' +data.cityName);
+			_cityKeys.push(data.cityKey);
+			_cityMap.push({'key': data.cityKey, 'name': data.cityName});			
+		}
+	});
+	
+	//city tags
+	var searchKey = '';
+	$(_cityMap).each(function(i){
+		var data = _cityMap[i];
+		if (!city || data.key == city.toUpperCase()) {			
+			if (!city) {
+				$('.searchcity').append('<li><a href="searchresult.html?city='+ data.key +'">' + data.name + '</a></li>');
+			}
+			else {
+				$('.searchcity').append('<li><a class="cur" href="searchresult.html?city='+ data.key +'">' + data.name + '</a></li>');
+				$('.searchresultBox .searchresultItem1 .col2 a').removeClass('cur');
+				if (!searchKey) {
+					searchKey = data.name;
+				}
+			}			
+					
+		}
+	});
+	
+	curpage = isNaN(parseInt(curpage)) ? 1 : parseInt(curpage);
+	var pagesize = 5;
+	//hospital table list
+	var numResult = 0;
+	$(_DATA).each(function(i){
+		var data = _DATA[i];
+		if (data.cityKey == city.toUpperCase()) {
+			numResult++;
+			if (numResult>(curpage-1)*pagesize && numResult<=curpage*pagesize) {
+				$('.searchresultTable').append('<tr><td class="col1">['+data.cityName+']</td><td class="col2">'+data.hospital+'</td><td class="col3"><a target="_blank" href="http://'+data.site+'">'+encodeURI(data.site)+'</a></td></tr>');			
+			}
+		}
+	});
+	
+	//head lables 
+	searchKey && $('#lblSearchKey').html(searchKey);
+	$('#lblSearchCnt').html(numResult);
+	
+	//page paging
+	$('.searchresultBox .pager').hide();
+	if (numResult > pagesize) {		
+		var pageNum = Math.ceil(numResult/pagesize);
+		var pageLink;
+		$('.searchresultBox .pager').html('');
+		
+		//prev page
+		if (curpage>1) {
+			pageLink = 'searchresult.html?city='+city+'&page='+(curpage-1);
+			$('.searchresultBox .pager').append('<a class="prev" href="'+ pageLink +'">Prev Page</a>');
+		}
+				
+		//pages 
+		for (var i=0;i<pageNum;i++) {
+			pageLink = 'searchresult.html?city='+city+'&page='+(i+1);
+			var curpageStyle = '';
+			if (curpage == (i+1)) {
+				pageLink = 'javascript:void(0);';
+				curpageStyle = 'style="background-color:#6db328"';
+			}
+			$('.searchresultBox .pager').append('<a class="nml" '+curpageStyle+' href="'+pageLink+'">'+(i+1)+'</a>');
+		}
+		
+		//next page
+		if (curpage<pageNum) {
+			pageLink = 'searchresult.html?city='+city+'&page='+(curpage+1);
+			$('.searchresultBox .pager').append('<a class="next" href="'+ pageLink +'">Next Page</a>');
+		}
+		$('.searchresultBox .pager').show();
+	}
+	
+  },//end searchresult
+  
+  //quiz.html
+  quiz: function(city, curpage) {
+	for(var i=1;i<=60;i++) {
+		$('#sel1').append('<option>myopia '+(i*25)+'D</option>');
+		$('#sel2').append('<option>hyperopia '+(i*25)+'D</option>');
+		$('#sel3').append('<option>astigmatism '+(i*25)+'D</option>');
+	}
+	
+	var $testLink =  $('.testilasikBox .testItem .answer a');
+	var $testSubmit = $('.testilasikBox .submit');
+	var answer1,
+	  answer2,
+	  answer3,
+	  answer4,
+	  answer5,
+	  answer6;
+
+	$testLink.on('click',function(e){
+		var curGroup;
+		var curScore;
+
+		$(this).siblings('a').removeClass('cur');
+		$(this).addClass('cur');
+		curGroup = $(this).data('group');
+		curScore = $(this).data('score');
+
+		switch(curGroup){
+			case 1:
+			answer1 = curScore;
+			break;
+			case 2:
+			answer2 = curScore;
+			break;
+			case 3:
+			answer3 = curScore;
+			break;
+			case 4:
+			answer4 = curScore;
+			break;
+			case 5:
+			answer5 = curScore;
+			if (answer5==6) {				
+				$('#divQuestionF').slideUp(500);
+				$('#divsel1,#divsel2,#divsel3').hide();
+			}
+			else {								
+				$('#divsel1,#divsel2,#divsel3').hide();
+				$('#divQuestionF').slideDown(500);				
+				if (answer5==1) {
+					$('#divsel1').show();
+				}
+				else if (answer5==2) {
+					$('#divsel2').show();
+				}
+				else if (answer5==3) {
+					$('#divsel3').show();
+				}
+				else if (answer5==4) {
+					$('#divsel1,#divsel3').show();
+				}
+				else if (answer5==5) {
+					$('#divsel2,#divsel3').show();
+				}
+			}
+			//alert(answer5);
+			break;
+			case 6:
+			answer6 = curScore;
+			break;
+		}
+		e.preventDefault();
+	});
+
+	$testSubmit.on('click',function(e){
+		if ( answer1 > 0 && answer2 > 0 && answer3 > 0 && answer4 > 0 && answer5 > 0 ){
+			if(answer5 == 6){
+				$('.testilasikBox .testilasikWrap').css({'display':'none'});
+				$('.testilasikBox .result1').css({'display':'block'});
+			}
+			else {
+				$('.testilasikBox .testilasikWrap').css({'display':'none'});
+				$('.testilasikBox .result2').css({'display':'block'});
+			}
+		}
+		else {
+			alert('Please answer all the question.')	
+		}
+		e.preventDefault(); 
+	});
+  }//end quiz
+};
